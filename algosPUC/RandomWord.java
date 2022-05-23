6e24fd195f8e7a08ec4b8841e00a9e0050e38a6b
carlitoslomon <calmos2266@gmail.com>
2022-05-22 23:42:14 -0600

    Fixed errors in HelloGoodbye and RandomWord

--- algosPUC/RandomWord.java
+++ algosPUC/RandomWord.java
@@ -3,12 +3,16 @@ import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Double count = 0.0;
+        String printWord = "";
         while (!(StdIn.isEmpty())) {
-            String tmp = StdIn.readString();
-            if (StdRandom.bernoulli())
-                StdOut.println(tmp);
+          count++; 
+          Double prob = 1.0/count;
+          String tmp = StdIn.readString();
+          if (StdRandom.bernoulli(prob))
+                printWord = tmp;
         }
-
+        StdOut.println(printWord);
     }
 }
